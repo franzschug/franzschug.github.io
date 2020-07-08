7eb670558318bd372eabd29e91eb33821e760e5c
Franz Schug <franz.schug@posteo.de>
2020-07-08 15:49:02 +0200

    Update features.js

--- features.js
+++ features.js
@@ -1,28 +1,31 @@
 
 $.getJSON("https://franzschug.github.io/data/populationdata.json", function(json) {
-	console.log(json)
   initChart(json);
 });
 	
 
 function initChart(json) {
-	window.onload = function() {
+	populations = []
+	densities = []
+	
+	for (var i = 0; i < json.length; i++) {
+		var object = json[i];
+		populations.push(object['EWZ'])
+		densities.push(object['EW_Dens'])
+}
+
+	let coords = populations.map( (v,i) => ({ x: v, y: densities[i]}) )
+	
+	data = [{ x: -8, y: 3 }, { x: 2, y: 8 }, { x: 3, y: 9 }]
+	//window.onload = function() {
+
 	var ctx = document.getElementById("chart1");
 	var scatterChart = new Chart(ctx, {
 		type: 'scatter',
 		data: {
 			datasets: [{
-				label: 'Scatter Dataset',
-				data: [{
-					x: -10,
-					y: 0
-				}, {
-					x: 0,
-					y: 10
-				}, {
-					x: 10,
-					y: 5
-				}]
+				label: 'Population / Population Density',
+				data: coords
 			}]
 		},
 		options: {
@@ -34,7 +37,7 @@ function initChart(json) {
 			}
 		}
 	});
-	}
+	//}
 }
 
 
